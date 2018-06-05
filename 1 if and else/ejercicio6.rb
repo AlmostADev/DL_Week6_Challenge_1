ab4a33c68878e9efed29dc81d1f35546095cd3d9
Alejandro Olmos <manuelalejandro.soto1@gmail.com>
2018-06-05 19:14:13 -0400

    Resolve some mistakes in exercises

--- 1 if and else/ejercicio6.rb
+++ 1 if and else/ejercicio6.rb
@@ -1,14 +1,14 @@
 # Utiliza álgebra booleana para hacer un refactoring de este código
 # Para verificar la respuestas, puedes modificar los valores de a y b.
 
-a = 'falso'
-b = 'verdadero'
+a = 'verdadero'
+b = 'falso'
 
-if a == 'verdadero' and b == 'falso'
+if a == 'verdadero' && b == 'falso'
 
   puts ':)'
 
-elsif a == 'verdadero' and b == 'verdadero'
+elsif a == 'verdadero' && b == 'verdadero'
 
   puts ':|'
 
