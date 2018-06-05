2b72ac6efd2ca167c057410bd9e587b27b7734e1
Alejandro Olmos <manuelalejandro.soto1@gmail.com>
2018-06-05 19:12:26 -0400

    Resolve some mistakes in exercises

--- 1 if and else/ejercicio5.rb
+++ 1 if and else/ejercicio5.rb
@@ -2,13 +2,13 @@
 # Para verificar la respuestas, puedes variar los valores de a y b.
 
 a = true
-b = false
+b = true
 
-if a == true and b == true
+if a == true && b == true
 
   puts 'Lograste A y B!'
 
-elsif a == true and b == false
+elsif a == false && b == true || a == true && b == false
 
   puts 'Lograste A! Pero no B!'
 
