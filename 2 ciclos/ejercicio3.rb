16af70300338a573fbf08db854d90813a720decc
Olmos Soto Manuel Alejandro <MSOLMOS@entel.cl>
2018-06-07 09:57:30 -0400

    Review item 1 and 2 of exercises to check that all works fine, still need review item 3

--- 2 ciclos/ejercicio3.rb
+++ 2 ciclos/ejercicio3.rb
@@ -1,19 +1,15 @@
 # Mostrar todos los divisores del número 990 con:
 # while, for, times.
 
-=begin
 counter = 1
 while counter <= 990
     puts counter if 990 % counter == 0
     counter += 1
 end
-=end
 
-=begin
 for i in 1..990
     puts i if 990 % i == 0
 end
-=end
 
 990.times do |i|
     puts i + 1 if 990 % (i + 1) == 0
